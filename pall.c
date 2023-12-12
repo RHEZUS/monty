c1e5a482ba5c107b0c865320c4f81e84f885cd35
hammed <hammedoyinkansola123@gmail.com>
2023-12-12 11:34:32 +0100

    monty

--- pall.c
+++ pall.c
@@ -1,8 +1,22 @@
 #include "monty.h"
 
+/**
+ * pall - prints all the values on the stack, starting from the top of the stack.
+ * @stack_t: stack header
+ * @line_number:
+*/
+
 void pall(stack_t **stack, unsigned int line_number)
 {
-    UNUSED (stack);
+    stack_t *head;
     UNUSED (line_number);
-    printf("pall\n");
+
+    head = *stack;
+    if (head == NULL)
+            return;
+    while (head)
+    {
+        printf("%d\n", head->n);
+        head = head->next
+    }    
 }
