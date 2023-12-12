2129dc3bc5be4aff60bfad8204ee10d5fb45ade4
hammed <hammedoyinkansola123@gmail.com>
2023-12-12 12:48:33 +0100

    monty

--- monty.h
+++ monty.h
@@ -58,6 +58,7 @@ extern bus_t bus;
 /******* FUNCTIONS *******/
 int execute(char *line, stack_t **stack, unsigned int line_number);
 void add_node(stack_t **head, int n);
+void nop(stack_t **stack, unsigned int line_number)
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
