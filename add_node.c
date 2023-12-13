a427728d513803739b28d127852a30ce896921f4
Seunda <ludivinseunda2@gmail.com>
2023-12-13 14:14:23 +0300

    solve mandatory tasks

--- add_node.c
+++ add_node.c
@@ -8,41 +8,19 @@
 void add_node(stack_t **stack, int n)
 {
 
-	stack_t *new_node = malloc(sizeof(stack_t));
+	stack_t *new_node = malloc(sizeof(stack_t)), *head = *stack;
 
-	if (stack == NULL)
-	{
-		fprintf(stderr, "Error\n");
-		exit(EXIT_FAILURE);
-	}
-
-	if (new_node == NULL)
-	{
-		fprintf(stderr, "Error\n");
-		exit(EXIT_FAILURE);
-	}
-
-	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = *stack;
-	*stack = new_node;
-	if (new_node->next != NULL)
-		new_node->next->prev = new_node;
-	/*
-	stack_t *new_node, *h;
-
-	h = *head;
 	new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
 	{
 		printf("Error\n");
 		exit(0);
 	}
-	if (h)
-		h->prev = new_node;
+	if (head)
+		head->prev = new_node;
 	new_node->n = n;
-	new_node->next = *head;
+	new_node->next = *stack;
 	new_node->prev = NULL;
-	*head = new_node;*/
+	*stack = new_node;
 }
 
