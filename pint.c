c23dcd0791184d6f8c95d1f7cb22cec415f105cd
Seunda <ludivinseunda2@gmail.com>
2023-12-13 12:04:50 +0300

    solve mandatory tasks

--- pint.c
+++ pint.c
@@ -12,7 +12,6 @@ void pint(stack_t **stack, unsigned int line_number)
 	if (stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
-		free_stack(*stack);
 		cleanup_and_exit();
 	}
 	printf("%d\n", (*stack)->n);
