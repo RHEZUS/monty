9a6b335156777647ec8078d963b3238404880b88
Seunda <ludivinseunda2@gmail.com>
2023-12-14 11:55:39 +0300

    solve mandatory tasks

--- pchar.c
+++ pchar.c
@@ -9,24 +9,24 @@
 
 void pchar(stack_t **stack, unsigned int line_number)
 {
-	if (!stack)
+	stack_t *head = stack;
+	if (!head)
 	{
-		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
-		free_stack(*stack);
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		fclose(bus.file);
 		free(bus.line);
+		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
-	if ((*stack)->n < 0 || (*stack)->n > 127)
+	if (head->n > 127 || head->n < 0)
 	{
-		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
-		free_stack(*stack);
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
 		fclose(bus.file);
 		free(bus.line);
+		free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
-
-	printf("%c\n", (*stack)->n);
+	printf("%c\n", head->n);
 
 }
 
