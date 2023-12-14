2b79f0da9ba1343fe8f8dbe71d11224799411624
Seunda <ludivinseunda2@gmail.com>
2023-12-14 10:55:32 +0300

    solve mandatory tasks

--- sub.c
+++ sub.c
@@ -20,7 +20,7 @@ void sub(stack_t **stack, unsigned int line_number)
 
 	if (len < 2)
 	{
-		fprintf(stderr, "L%d: can't sub, stack too short,\n", line_number);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		free_stack(*stack);
 		fclose(bus.file);
 		free(bus.line);
