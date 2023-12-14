b188e05bda4707b0ba69d54389b3243917334528
Seunda <ludivinseunda2@gmail.com>
2023-12-14 11:50:04 +0300

    solve mandatory tasks

--- pchar.c
+++ pchar.c
@@ -17,7 +17,7 @@ void pchar(stack_t **stack, unsigned int line_number)
 		free(bus.line);
 		exit(EXIT_FAILURE);
 	}
-	if ((*stack)->n <= 127)
+	if ((*stack)->n > 0 && (*stack)->n <= 127)
 	{
 		printf("%c\n", (*stack)->n);
 	}
